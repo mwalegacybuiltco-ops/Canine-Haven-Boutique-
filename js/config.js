e4c5b0b643c7a29fb193df12eba21765317af59a
mwalegacybuiltco-ops <mwalegacybuiltco@gmail.com>
2026-01-05 09:45:14 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
   starterPackSquarePayLink: "PASTE_YOUR_STARTER_PACK_$140_SQUARE_PAYMENT_LINK_HERE",
   customerFormEmbedUrl: "PASTE_CUSTOMER_GOOGLE_FORM_EMBED_SRC_HERE",
   affiliateFormEmbedUrl: "PASTE_AFFILIATE_GOOGLE_FORM_EMBED_SRC_HERE",
-  customerFormPrefillTemplate: "",
+  customerFormPrefillTemplate: "https://docs.google.com/forms/d/e/1FAIpQLSeh83XNLSpXh4-5-9N40RmPp37OFdxnGA2D95T7Q3CBy5hoQg/viewform?usp=header",
   affiliateFormPrefillTemplate: "",
   affiliateStatsUrl: "PASTE_YOUR_READ_ONLY_STATS_PAGE_LINK_HERE",
   facebookGroupUrl: "PASTE_YOUR_FACEBOOK_GROUP_LINK_HERE"
